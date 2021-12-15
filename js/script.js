c07d110a858a8e416c6def3121d379c839475f6f
Caro-L-dev <liardcar@gmail.com>
2021-12-15 22:53:28 +0100

    :construction: chore(all): Work In Progress

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 /* ============================== Typing Animation ============================ */
-var typed = new Typed(".typing",{
-    strings:["","Oune artiste","Web Developer","Graphic Designer"],
+let typed = new Typed(".typing",{
+    strings:["","une web dev' en devenir !","oune artiste","une étudiante dans la promo Z n°3",", je suis ?"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
 });
+
+/* ============================== Aside Active Tab ============================ */
+const nav = document.querySelector(".nav"),
+        navList = nav.querySelectorAll("li"),
+        totalNavList = navList.length;
+        for(let index=0; index < totalNavList; index++) {
+           // console.log(navList[index]);
+           const a = navList[i].querySelector("a");
+           a.addEventListener("click", function(){
+               for(let j=0; j < totalNavList; j++){
+                   navList[j].querySelector("a").classList.remove("active");
+               }
+               this.classList.add("active");
+           })
+        };
--- js/style-switcher.js
+++ js/style-switcher.js
@@ -2,14 +2,16 @@
 const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 styleSwitcherToggle.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
-})
-// hide style - switcher on scroll
+});
+
+// Hide style - switcher on scroll
 window.addEventListener("scroll", () => {
     if(document.querySelector(".style-switcher").classList.contains("open"))
     {
         document.querySelector(".style-switcher").classList.remove("open");
     }
-})
+});
+
 /* ========================== Theme Colors =========================== */
 const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color)
@@ -24,14 +26,16 @@ function setActiveStyle(color)
             style.setAttribute("disabled","true");
         }
     })
-}
+};
+
 /* ========================== Theme Light & Dark Mode =========================== */
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
     document.body.classList.toggle("dark");
-})
+});
+
 window.addEventListener("load", () => {
     if(document.body.classList.contains("dark"))
     {
@@ -41,4 +45,4 @@ window.addEventListener("load", () => {
     {
         dayNight.querySelector("i").classList.add("fa-moon");
     }
-})
+});
